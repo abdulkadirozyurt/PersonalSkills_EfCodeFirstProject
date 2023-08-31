a698ef87726015fb6608ecc5a8c2d778e541b355
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-08-31 16:44:25 +0300

    Get skills from database

--- PersonalSkills_EfCodeFirstProject/Controllers/SkillsController.cs
+++ PersonalSkills_EfCodeFirstProject/Controllers/SkillsController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonalSkills_EfCodeFirstProject.Models.Contexts;
 
 namespace PersonalSkills_EfCodeFirstProject.Controllers
 {
-    public class DefaultController : Controller
+    public class SkillsController : Controller
     {
         // GET: Default
+        private SkillsContext skillsContext = new SkillsContext();
         public ActionResult Index()
         {
-            return View();
+            var values = skillsContext.Skills.ToList();
+            return View(values);
         }
     }
 }

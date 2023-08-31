63c8f0fae3c94248ade504781aafe2b53345b989
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-08-31 17:52:59 +0300

    List Skilss In Admin

---
+++ PersonalSkills_EfCodeFirstProject/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonalSkills_EfCodeFirstProject.Models.Contexts;
+
+namespace PersonalSkills_EfCodeFirstProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        SkillsContext skillsContext= new SkillsContext();
+        public ActionResult Index()
+        {
+            var skills=skillsContext.Skills.ToList();
+
+            return View(skills);
+        }
+    }
+}

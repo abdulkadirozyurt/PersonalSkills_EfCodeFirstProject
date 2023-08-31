c554d3d32af72ee0725ddd8b628d7a32f52f371b
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-08-31 15:42:49 +0300

    database created with code first

---
+++ PersonalSkills_EfCodeFirstProject/Models/Contexts/SkillsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using PersonalSkills_EfCodeFirstProject.Models.Entities;
+
+namespace PersonalSkills_EfCodeFirstProject.Models.Contexts
+{
+    public class SkillsContext : DbContext
+    {
+
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
---
+++ PersonalSkills_EfCodeFirstProject/Models/Entities/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalSkills_EfCodeFirstProject.Models.Entities
+{
+    public class Skill
+    {
+        [Key]
+        public int  Id { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+    }
+}
